4a0e993b4292de0d3b2c0980d7c798e69918d26b
UldrenWEB <uldrenmiguel33@gmail.com>
2024-07-06 05:59:47 -0400

    fasdf

--- src/controllers/Playlist/playlistModifyController.js
+++ src/controllers/Playlist/playlistModifyController.js
@@ -1,42 +1,56 @@
-import Playlist from '../../models/Playlist.js';
+import Playlist from "../../models/Playlist.js";
 
 export const modifyPlaylist = async (req, res) => {
-    try {
-        // Extraer la playlist a modificar
-        const { idPlaylist, name, image, idSong } = req.body;
-        const playlist = await Playlist.findById(idPlaylist);
+  try {
+    const { idPlaylist, idSong } = req.body;
 
-        if (!playlist) {
-            return res.status(404).json({ message: { code: 404, description: 'Playlist no encontrada' } });
-        }
-
-        // Verificar si el userId de la playlist coincide con el userId del usuario autenticado
-        if (playlist.userId.toString()!== req.user._id.toString()) {
-            return res.status(403).json({ message: { code: 403, description: 'Acceso denegado: No tienes permiso para modificar esta playlist.' } });
-        }
-
-        // Modificar la playlist
-        playlist.name = name;
-        playlist.image = image;
-        playlist.idSong = idSong;
-
-        // Guardar la playlist modificada
-        const savedPlaylist = await playlist.save();
-
-        // Formatear la respuesta según las nuevas especificaciones
-        const response = {
-            message: {
-                code: 200,
-                description: 'Playlist modificada correctamente'
-            },
-            data: {
-                playlist: savedPlaylist
-            }
-        };
-
-        res.status(200).json(response);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: { code: 500, description: 'Error interno del servidor' }, error: error.message });
+    const playlist = await Playlist.findById(idPlaylist);
+    if (!playlist) {
+      return res.status(404).json({
+        message: { code: 404, description: "Playlist no encontrada" },
+      });
     }
-}
+    if (playlist.userId.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        message: {
+          code: 403,
+          description:
+            "Acceso denegado: No tienes permiso para modificar esta playlist.",
+        },
+      });
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+      idPlaylist,
+      { $push: { idSong: idSong } },
+      { new: true }
+    );
+
+    if (!updatedPlaylist) {
+      return res.status(404).json({
+        message: {
+          code: 404,
+          description: "No se pudo actualizar la playlist",
+        },
+      });
+    }
+
+    const response = {
+      message: {
+        code: 200,
+        description: "Canción agregada a la playlist correctamente",
+      },
+      data: {
+        playlist: updatedPlaylist,
+      },
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: { code: 500, description: "Error interno del servidor" },
+      error: error.message,
+    });
+  }
+};
