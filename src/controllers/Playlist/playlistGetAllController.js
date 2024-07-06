81b3bc123ad96fa5ee29fe55f8618776a771eed2
UldrenWEB <uldrenmiguel33@gmail.com>
2024-07-06 06:00:42 -0400

    fasd

--- src/controllers/Playlist/playlistGetAllController.js
+++ src/controllers/Playlist/playlistGetAllController.js
@@ -1,28 +1,28 @@
 // src/controllers/PlaylistController.js
-import Playlist from '../../models/Playlist.js';
+import Playlist from "../../models/Playlist.js";
 
 class PlaylistGetController {
   async getUserPlaylists(req, res) {
     try {
       const userId = req.user._id; // Obtén el ID del usuario autenticado desde req.user
 
-      const playlists = await Playlist.find({ userId }).populate('idSong');
+      const playlists = await Playlist.find({ userId }).populate("idSong");
 
       return res.status(200).json({
         message: {
-          description: 'Playlists retrieved successfully',
-          code: 0
+          description: "Playlists retrieved successfully",
+          code: 0,
         },
-        data: playlists
+        data: playlists,
       });
     } catch (error) {
       console.error(error);
       return res.status(500).json({
         message: {
-          description: 'Internal server error',
-          code: 1
+          description: "Internal server error",
+          code: 1,
         },
-        data: null
+        data: null,
       });
     }
   }
